8c387342c224af2ae6c4b2a095f5120ddb5f1447
Ladislav Soukup <ladislav.soukup@gmail.com>
2022-08-01 12:31:24 +0200

    startup  info

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import filesCleanup from "./workers/filesCleanup.js";
 import configFetch from "./workers/configFetch.js";
 import SessionManager from "./session-manager.js";
 import routes from "./routes.js";
+import serverInfo from "./api/serverInfo.js";
+
+const server = express();
+const srvInfo = serverInfo();
 
 // GLOBAL var
 global.sessions = new SessionManager();
@@ -20,29 +24,44 @@ Logger.log("██╔══██║██║     ╚════██║");
 Logger.log("██║  ██║███████╗███████║");
 Logger.log("╚═╝  ╚═╝╚══════╝╚══════╝");
 Logger.log("   SPECTADO HLS RELAY   ");
+
 Logger.log("------------------------------------------------");
-Logger.log("Starting the server...");
+Logger.log(srvInfo.server);
+Logger.log(`version: ${srvInfo.version}`);
+Logger.log(`${srvInfo.platform} - ${srvInfo.arch}`);
+Logger.log("------------------------------------------------");
 
-const server = express();
-http.createServer(server).listen(config.http.port);
-Logger.log(`HTTP  listening on port ${config.http.port}`);
-
-if (config.https.port) {
-  const httpsOptions = {
-    key: fs.readFileSync(config.https.key, "utf8"),
-    cert: fs.readFileSync(config.https.cert, "utf8"),
-  };
-
-  https.createServer(httpsOptions, server).listen(config.https.port);
-  Logger.log(`HTTPS listening on port ${config.https.port}`);
+try {
+  http.createServer(server).listen(config.http.port, () => {
+    Logger.log(`HTTP  listening on port ${config.http.port}`);
+  });
+} catch (err) {
+  Logger.warn(`HTTP error - can't start on port ${config.https.port}`);
+  Logger.debug(err);
 }
 
-Logger.log(`FFMPEG binary path ${config.ffmpeg}`);
+try {
+  if (config.https.port) {
+    const httpsOptions = {
+      key: fs.readFileSync(config.https.key, "utf8"),
+      cert: fs.readFileSync(config.https.cert, "utf8"),
+    };
 
-Logger.log(`Started cleanup worker - path: ${config.hls.root}`);
+    https.createServer(httpsOptions, server).listen(config.https.port, () => {
+      Logger.log(`HTTPS listening on port ${config.https.port}`);
+    });
+  }
+} catch (err) {
+  Logger.warn(`HTTPS error - can't start on port ${config.https.port}`);
+  Logger.debug(err);
+}
+
+Logger.log(`FFMPEG binary path is "${config.ffmpeg}"`);
+
+Logger.log(`Started cleanup worker on path "${config.hls.root}"`);
 filesCleanup();
 
-Logger.log(`Started config fetch worker - path: ${config.streamSource}`);
+Logger.log(`Started config fetch worker from source "${config.streamSource}"`);
 configFetch();
 
 // init express web routes
